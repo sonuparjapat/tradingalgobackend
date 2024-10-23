e986e049060791f5397fed2ca21636d5e381fcdb
sonuparjapat <sahil992001parjapat@gmail.com>
2024-10-23 15:23:58 +0530

    changes

---
+++ data/Utility/clientInfo.js
@@ -0,0 +1,20 @@
+const internalIp = require('internal-ip');
+const publicIp = require('public-ip');
+const macaddress = require('macaddress');
+
+export const getClientInfo = async () => {
+  try {
+    const localIP = await internalIp.v4();
+    const publicIP = await publicIp.v4();
+    const macAddress = await macaddress.one();
+
+    return {
+      localIP,
+      publicIP,
+      macAddress
+    };
+  } catch (error) {
+    console.error('Error fetching client info:', error);
+    throw new Error('Unable to retrieve client information.');
+  }
+};
